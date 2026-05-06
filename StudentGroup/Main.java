311874f1e27bb530317d1f2323d2137cabf5b539
MartinShitc99 <135505202+MartinShitc99@users.noreply.github.com>
2026-05-06 19:18:56 +0500

    Update Main.java

--- StudentGroup/Main.java
+++ StudentGroup/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         StudentGroup studentGroup = new StudentGroup("Furer", 14);
 
-        studentGroup.studentCount = 88;
+        studentGroup.setStudentCount(88);
 
         studentGroup.printInfo();
     }
