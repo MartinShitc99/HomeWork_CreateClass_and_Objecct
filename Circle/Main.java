4c9679c10eb33b3bd454d4ac32a4f0007def89af
MartinShitc99 <135505202+MartinShitc99@users.noreply.github.com>
2026-05-06 19:16:09 +0500

    Update Main.java

--- Circle/Main.java
+++ Circle/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle(14.77);
 
-        circle.radius = (14.99);
+        circle.setRadius(2.2);
+        
         System.out.println("Площадь: " + circle.calculateArea());
         System.out.println("Длина: " + circle.calculateCircumference());
     }
