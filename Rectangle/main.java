960a32faf15b0550940f3680aa79b7ae07a1b384
MartinShitc99 <135505202+MartinShitc99@users.noreply.github.com>
2026-05-06 19:17:34 +0500

    Update main.java

--- Rectangle/main.java
+++ Rectangle/main.java
@@ -6,7 +6,7 @@ public class main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle( 200.0, 200.2);
 
-        rectangle.width = 211.1;
+        rectangle.setWidth(777.7);
 
         out.println(rectangle.width * rectangle.height);
 
