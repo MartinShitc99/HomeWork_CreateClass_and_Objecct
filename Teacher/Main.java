b90f8567750641feba76faa5f35decd9718f1239
MartinShitc99 <135505202+MartinShitc99@users.noreply.github.com>
2026-05-06 19:19:25 +0500

    Update Main.java

--- Teacher/Main.java
+++ Teacher/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Teacher teacher = new Teacher("Наталья", "Русский язык");
 
-        teacher.subject = "Немецкий язык";
+        teacher.setSubject("Немецкий язык");
 
         teacher.print();
     }
